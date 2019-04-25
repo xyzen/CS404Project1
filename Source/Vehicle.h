c854a5f13f16e9f38ac1dc761f7d817ff024c8ca
michaelcu <43478115+michaelcu@users.noreply.github.com>
2019-04-25 16:45:00 -0500

    Update Vehicle.h
    Added Functions and Made Variables Private

--- Source/Vehicle.h
+++ Source/Vehicle.h
@@ -4,9 +4,14 @@ class Vehicle
 public:
 	Vehicle();
 	~Vehicle();
+	int getID();
+	int getType();
+	int getZipCode();
+	bool getAvail();
+private:
 	int ID;
 	int type;
 	int zipCode;
-	bool availability;
+	bool avail;
 };
 
