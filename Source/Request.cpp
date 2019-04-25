2dca8c05db1fb0eae3b6c5e88517702e5a0bf0a2
michaelcu <43478115+michaelcu@users.noreply.github.com>
2019-04-25 16:51:01 -0500

    Update Request.cpp
    Added Getters for Request variables, ID, Type, ZipCode

--- Source/Request.cpp
+++ Source/Request.cpp
@@ -10,3 +10,18 @@ Request::Request()
 Request::~Request()
 {
 }
+
+int Request::getID()
+{
+  return ID;
+}
+
+int Request::getType()
+{
+  return type;
+}
+
+int Request::getZipCode()
+{
+  return zipCode;
+}
