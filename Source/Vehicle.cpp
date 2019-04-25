ba22250fe0deb8922f1e18a10a23c4e6979e4f05
michaelcu <43478115+michaelcu@users.noreply.github.com>
2019-04-25 16:47:51 -0500

    Update Vehicle.cpp
    Added Getterrs for ID, Type, and ZipCode.

--- Source/Vehicle.cpp
+++ Source/Vehicle.cpp
@@ -10,3 +10,18 @@ Vehicle::Vehicle()
 Vehicle::~Vehicle()
 {
 }
+
+int Vehicle::getID()
+{
+  return ID;
+}
+
+int Vehicle::getType()
+{
+  return type;
+}
+
+int Vehicle::getZipCode()
+{
+  return zipCode;
+}
