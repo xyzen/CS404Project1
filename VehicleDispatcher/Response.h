0cacece1d753a0af4eda3e69d5da49724070c3ef
Th3xx <49999173+Th3xx@users.noreply.github.com>
2019-05-08 19:17:24 -0500

    added reqid

--- VehicleDispatcher/Response.h
+++ VehicleDispatcher/Response.h
@@ -3,7 +3,7 @@
 
 struct Response
 {
-	Response(unsigned int type, unsigned int zip, unsigned int id, unsigned int dist, std::string route);
-	unsigned int type, zip, id, dist;
+	Response(unsigned int reqID, unsigned int type, unsigned int zip, unsigned int id, unsigned int dist, std::string route);
+	unsigned int type, zip, id, dist, reqID;
 	std::string route;
 };
